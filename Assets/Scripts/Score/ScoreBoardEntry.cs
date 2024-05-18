b40651bcf965d12c0c986162cd1d1449e9742289
Dzaky Fattan <dzaky.fattan@gmail.com>
2024-05-18 12:49:25 +0700

    Add basic scoreboard script

---
+++ Assets/Scripts/Score/ScoreBoardEntry.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreBoardEntry : MonoBehaviour
+{
+    // data class for scoreboard entry
+    public TextMeshProUGUI NameText { get; private set; }
+    public TextMeshProUGUI DateTimeText { get; private set; }
+    public TextMeshProUGUI ScoreValueText { get; private set; } // for now it's time, but it can be anything
+
+    public ScoreBoardEntry(string name, DateTime dateTime, float scoreValue)
+    {
+        this.NameText.text = name;
+        // format date to DD/MM/YYYY
+        this.DateTimeText.text = dateTime.ToString("dd-MM-yyyy HH:mm:ss");
+        this.ScoreValueText.text = scoreValue.ToString();
+    }
+}
