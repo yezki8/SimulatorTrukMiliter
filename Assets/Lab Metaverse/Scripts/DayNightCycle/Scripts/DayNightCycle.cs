ac652a413f8dc8a5c4ff61f5e2efb1bbeeaa919e
yezki8 <yehezkiel.live@gmail.com>
2024-05-24 19:50:54 +0700

    Add "restart" feature in day night cycle

--- Assets/Lab Metaverse/Scripts/DayNightCycle/Scripts/DayNightCycle.cs
+++ Assets/Lab Metaverse/Scripts/DayNightCycle/Scripts/DayNightCycle.cs
@@ -9,15 +9,11 @@ public class DayNightCycle : MonoBehaviour
 
     void Update()
     {
-        //Make sure that day clock only run when game is active
-        if (GameStateController.Instance.GameState == StateOfGame.Match)
+        if (dayTimeManager != null && sun != null)
         {
-            if (dayTimeManager != null && sun != null)
-            {
-                // Calculate the sun's rotation based on the time of day
-                float sunAngle = (dayTimeManager.timeOfDay / 24f) * 360f;
-                sun.transform.rotation = Quaternion.Euler(new Vector3(sunAngle - 90, 170, 0));
-            }
+            // Calculate the sun's rotation based on the time of day
+            float sunAngle = (dayTimeManager.timeOfDay / 24f) * 360f;
+            sun.transform.rotation = Quaternion.Euler(new Vector3(sunAngle - 90, 170, 0));
         }
     }
 }
--- Assets/Lab Metaverse/Scripts/DayNightCycle/Scripts/DayTimeManager.cs
+++ Assets/Lab Metaverse/Scripts/DayNightCycle/Scripts/DayTimeManager.cs
@@ -5,13 +5,37 @@ using TMPro;
 
 public class DayTimeManager : MonoBehaviour
 {
+    //TODO: Convert this float to Scriptable object when needed in the future
+    [Range(0, 24)]
+    public float DefaultDayStart = 9;   //The hour of the game started
+    
     [Range(0, 24)]
     public float timeOfDay;  // Current time of day (0-24)
     public float dayDuration = 120f;  // Duration of a full day in seconds
     public int currentDay = 1;
     public TextMeshProUGUI timeText;  // Time Text UI
 
+    private void Start()
+    {
+        InitializeDayTime();
+    }
+
     void Update()
+    {
+        //Make sure that day clock only run when game is active
+        if (GameStateController.Instance.GameState == StateOfGame.Match)
+        {
+            CalculateDayTime();
+        }
+    }
+
+    public void InitializeDayTime()
+    {
+        timeOfDay = DefaultDayStart;
+        CalculateDayTime();
+    }
+
+    void CalculateDayTime()
     {
         // Calculate how much time to add to timeOfDay this frame
         timeOfDay += (24 / dayDuration) * Time.deltaTime;
