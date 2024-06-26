0606579a15dec8558d0f9f813abe9c3c9163fd95
Dzaky Fattan <dzaky.fattan@gmail.com>
2024-06-26 16:36:15 +0700

    feat: button to change weather in intro

--- Assets/Lab Metaverse/_Scripts/WeatherSystem/WeatherSystem.cs
+++ Assets/Lab Metaverse/_Scripts/WeatherSystem/WeatherSystem.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 public class WeatherSystem : MonoBehaviour
 {
-    private enum WeatherType
+    public enum WeatherType
     {
         Sunny,
         Cloudy,
         Rainy,
         // Snowy,
     }
-    [SerializeField] private WeatherType _currentWeather = WeatherType.Sunny;
+    public WeatherType CurrentWeather { get; private set; }
     // player to get the player's position
     [SerializeField] private GameObject _player;
     // rain spawner gameobject
@@ -39,37 +39,75 @@ public class WeatherSystem : MonoBehaviour
         }
     }
 
+    // manage rain spawner activation
+    private void HandleRainSpawner()
+    {
+        if (CurrentWeather == WeatherType.Rainy)
+        {
+            _rainSpawner.SetActive(true);
+        }
+        else
+        {
+            _rainSpawner.SetActive(false);
+        }
+    }
+
+    public WeatherType GetCurrentWeather()
+    {
+        return CurrentWeather;
+    }
+
     private void setWeather(int weatherState)
     {
         switch (weatherState)
         {
             case 0:
-                _currentWeather = WeatherType.Sunny;
-                _rainSpawner.SetActive(false);
+                CurrentWeather = WeatherType.Sunny;
                 break;
             case 1:
-                _currentWeather = WeatherType.Cloudy;
-                _rainSpawner.SetActive(false);
+                CurrentWeather = WeatherType.Cloudy;
                 break;
             case 2:
-                _currentWeather = WeatherType.Rainy;
-                _rainSpawner.SetActive(true);
+                CurrentWeather = WeatherType.Rainy;
                 break;
             // case 3:
             //     _currentWeather = WeatherType.Snowy;
             //     break;
             default:
-                _currentWeather = WeatherType.Sunny;
-                _rainSpawner.SetActive(false);
+                CurrentWeather = WeatherType.Sunny;
                 break;
         }
-        Debug.Log("Weather set to " + _currentWeather);
+        HandleRainSpawner();
+        Debug.Log("Weather set to " + CurrentWeather);
+    }
+
+    // single button control to change weather
+    public void ChangeWeather()
+    {
+        switch (CurrentWeather)
+        {
+            case WeatherType.Sunny:
+                setWeather(1);
+                break;
+            case WeatherType.Cloudy:
+                setWeather(2);
+                break;
+            case WeatherType.Rainy:
+                setWeather(0);
+                break;
+            // case WeatherType.Snowy:
+            //     setWeather(0);
+            //     break;
+            default:
+                setWeather(0);
+                break;
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-
+        setWeather(0);
     }
 
     // Update is called once per frame
