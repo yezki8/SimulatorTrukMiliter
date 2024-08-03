7ab8653e2bdeece0e4e9fe7bae3884fec60c0855
Hughie Alghaniyyu Emiliano <68457533+HughieAlghani@users.noreply.github.com>
2024-08-03 20:50:47 +0700

    set connection object layer to road and add road segment component

--- Assets/Lab Metaverse/_Scripts/Road/AutoAddRoadSegment.cs
+++ Assets/Lab Metaverse/_Scripts/Road/AutoAddRoadSegment.cs
@@ -17,12 +17,14 @@ public class AutoAddRoadSegment
     {
         // Call this method every frame to ensure new objects are processed
         ConfigureRoadObject();
+        ConfigureConnectionObject();
     }
 
     private static void OnHierarchyChanged()
     {
         // Call this method when hierarchy changes
         ConfigureRoadObject();
+        ConfigureConnectionObject();
     }
 
     private static void ConfigureRoadObject()
@@ -44,6 +46,37 @@ public class AutoAddRoadSegment
         }
 
         foreach (Transform child in roadObjects.transform)
+        {
+            // Add the RoadSegment script if it does not already have it
+            if (child.GetComponent<RoadSegment>() == null)
+            {
+                child.gameObject.AddComponent<RoadSegment>();
+            }
+
+            // Set the layer to "Road"
+            child.gameObject.layer = roadLayer;
+        }
+    }
+
+    private static void ConfigureConnectionObject()
+    {
+        // Define the layer name and get its index
+        int roadLayer = LayerMask.NameToLayer("Road");
+
+        // Ensure the layer exists
+        if (roadLayer == -1)
+        {
+            Debug.LogError($"Layer 'Road' does not exist. Please create it in the Tags and Layers settings.");
+            return;
+        }
+
+        GameObject connectionObjects = GameObject.Find("Connection Objects");
+        if (connectionObjects == null)
+        {
+            return;
+        }
+
+        foreach (Transform child in connectionObjects.transform)
         {
             // Add the RoadSegment script if it does not already have it
             if (child.GetComponent<RoadSegment>() == null)
