94dbd69a9433df2a640a8c9c38c13a5084d4a8e8
Hughie Alghaniyyu Emiliano <68457533+HughieAlghani@users.noreply.github.com>
2024-08-22 15:33:27 +0700

    change camera offset variable name

--- Assets/Lab Metaverse/_Scripts/MiniMap/MiniMap.cs
+++ Assets/Lab Metaverse/_Scripts/MiniMap/MiniMap.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class MiniMap : MonoBehaviour
 {
     [SerializeField] private Transform player;
-    [SerializeField] private Vector3 offset = new Vector3(0, 15, 0);
+    [SerializeField] private Vector3 cameraOffset = new Vector3(0, 15, 0);
 
     void Start()
     {
-        transform.position = player.position + offset;
+        transform.position = player.position + cameraOffset;
     }
 
     void LateUpdate()
     {
-        transform.position = player.position + offset;
+        transform.position = player.position + cameraOffset;
         transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
     }
 }
