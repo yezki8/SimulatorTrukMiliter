d16a2d6bbc3885fd9184b57a62c2af5bd9d481d9
Hughie Alghaniyyu Emiliano <68457533+HughieAlghani@users.noreply.github.com>
2024-08-24 21:40:46 +0700

    add big map camera script

---
+++ Assets/Lab Metaverse/_Scripts/MiniMap/BigMapCamera.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigMapCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        gameObject.SetActive(false);
+    }
+}
