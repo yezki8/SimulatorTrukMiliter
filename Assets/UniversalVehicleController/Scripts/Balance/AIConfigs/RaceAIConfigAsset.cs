8ee39590b37ac8484791116c897fdeba664891f2
MSI-GL65\dzaky <dzaky.fattan@gmail.com>
2024-09-09 14:04:14 +0700

    feat: bikes, wider vehicle view checks

--- Assets/UniversalVehicleController/Scripts/Balance/AIConfigs/RaceAIConfigAsset.cs
+++ Assets/UniversalVehicleController/Scripts/Balance/AIConfigs/RaceAIConfigAsset.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace PG
 {
-    [CreateAssetMenu (fileName = "RaceAIConfig", menuName = "AI/RaceAIConfigAsset")]
+    [CreateAssetMenu (fileName = "SimAIConfig", menuName = "AI/RaceAIConfigAsset")]
     public class RaceAIConfigAsset :BaseAIConfigAsset
     {
         public RaceAIConfig RaceAIConfig;
