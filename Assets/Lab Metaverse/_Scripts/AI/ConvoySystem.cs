e535700090dd06875235146ddb84ce27caad884f
Dzaky Fattan Rizqullah <dzaky.fattan@gmail.com>
2024-09-10 13:07:45 +0700

    feat: convoy system gameObject

---
+++ Assets/Lab Metaverse/_Scripts/AI/ConvoySystem.cs
@@ -0,0 +1,22 @@
+using PG;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConvoySystem : MonoBehaviour
+{
+    // List of all vehicles in the convoy
+    public List<BaseAIControl> vehicleControls = new();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
