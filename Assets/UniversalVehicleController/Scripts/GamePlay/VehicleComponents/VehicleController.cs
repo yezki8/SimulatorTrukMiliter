b3a48ecbfd5b4e757bfc8a886addf8163d6996b1
Dzaky Fattan Rizqullah <dzaky.fattan@gmail.com>
2024-10-01 16:37:24 +0700

    feat: switch lighting when night for npc vehicles

--- Assets/UniversalVehicleController/Scripts/GamePlay/VehicleComponents/VehicleController.cs
+++ Assets/UniversalVehicleController/Scripts/GamePlay/VehicleComponents/VehicleController.cs
@@ -191,7 +191,6 @@ namespace PG
 
             RB.velocity = Vector3.zero;
             RB.angularVelocity = Vector3.zero;
-            RB.isKinematic = true;
 
             //float y = transform.rotation.eulerAngles.y;
             //this.GetComponent<Rigidbody>().position += Vector3.up * 2f;
