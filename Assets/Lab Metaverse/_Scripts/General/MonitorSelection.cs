91bd342b1f384b0caae89b6ac66f5d63cdb60bb9
Hughie Alghaniyyu Emiliano <68457533+HughieAlghani@users.noreply.github.com>
2024-10-11 13:07:39 +0700

    Update Monitor Selection Algorithm

--- Assets/Lab Metaverse/_Scripts/General/MonitorSelection.cs
+++ Assets/Lab Metaverse/_Scripts/General/MonitorSelection.cs
@@ -7,7 +7,20 @@ using TMPro;
 public class MonitorSelection : MonoBehaviour
 {
     [SerializeField] private GameObject _setDisplayPanel;
+    [SerializeField] private Camera FrontViewCamera;
+    [SerializeField] private Camera LeftMirrorCamera;
+    [SerializeField] private Camera RightMirrorCamera;
+    [SerializeField] private Camera RearMirrorCamera;
+    [SerializeField] private Camera MainCamera;
+    [SerializeField] private Canvas InstructorDisplay;
+    [SerializeField] private Canvas LeftMirrorDisplay;
+    [SerializeField] private Canvas RightMirrorDisplay;
+    [SerializeField] private Canvas RearMirrorDisplay;
+    [SerializeField] private Canvas DashboardDisplay;
+
     public TMP_Dropdown[] displayDropdowns = new TMP_Dropdown[6];
+
+    // Assign cameras and canvases for each display
 
     void Start()
     {
@@ -15,6 +28,7 @@ public class MonitorSelection : MonoBehaviour
         PopulateMonitorDropdowns();
 
         Debug.Log($"Unity detected {Display.displays.Length} displays.");
+        // Activate as many displays as Unity detects (up to 6 for your setup)
         for (int i = 0; i < Mathf.Min(Display.displays.Length, 6); i++)
         {
             Display.displays[i].Activate();
@@ -22,7 +36,6 @@ public class MonitorSelection : MonoBehaviour
         }
     }
 
-    // Populate the monitor dropdowns with monitor indices
     void PopulateMonitorDropdowns()
     {
         for (int i = 0; i < displayDropdowns.Length; i++)
@@ -39,26 +52,53 @@ public class MonitorSelection : MonoBehaviour
         }
     }
 
-    public void OnMonitorSelected(int displayIndex)
+    public void SetFrontViewCamera(int monitorIndex)
     {
-        int monitorIndex = displayDropdowns[displayIndex].value;
-
-        if (monitorIndex < Display.displays.Length)
-        {
-            Display.displays[monitorIndex].Activate();
-            Debug.Log($"Display {displayIndex + 1} is now using Monitor {monitorIndex + 1}.");
-        }
-        else
-        {
-            Debug.LogError($"Monitor {monitorIndex + 1} not available.");
-        }
+        FrontViewCamera.targetDisplay = monitorIndex + 1;
+        Display.displays[monitorIndex].Activate();
     }
 
+    public void SetInstructorDisplay(int monitorIndex)
+    {
+        MainCamera.targetDisplay = monitorIndex + 1;
+        InstructorDisplay.targetDisplay = monitorIndex + 1;
+        Display.displays[monitorIndex].Activate();
+    }
+
+    public void SetLeftMirrorDisplay(int monitorIndex)
+    {
+        LeftMirrorCamera.targetDisplay = monitorIndex + 1;
+        LeftMirrorDisplay.targetDisplay = monitorIndex + 1;
+        Display.displays[monitorIndex].Activate();
+    }
+
+    public void SetRightMirrorDisplay(int monitorIndex)
+    {
+        RightMirrorCamera.targetDisplay = monitorIndex + 1;
+        RightMirrorDisplay.targetDisplay = monitorIndex + 1;
+        Display.displays[monitorIndex].Activate();
+    }
+
+    public void SetRearMirrorDisplay(int monitorIndex)
+    {
+        RearMirrorCamera.targetDisplay = monitorIndex + 1;
+        RearMirrorDisplay.targetDisplay = monitorIndex + 1;
+        Display.displays[monitorIndex].Activate();
+    }
+
+    public void SetTruckDashboardDisplay(int monitorIndex)
+    {
+        DashboardDisplay.targetDisplay = monitorIndex + 1;
+        Display.displays[monitorIndex].Activate();
+    }
+
+    // Open the display setting panel
     public void OpenDisplaySettingPanel()
     {
         _setDisplayPanel.SetActive(true);
     }
 
+    // Close the display setting panel
     public void CloseDisplaySettingPanel()
     {
         _setDisplayPanel.SetActive(false);
