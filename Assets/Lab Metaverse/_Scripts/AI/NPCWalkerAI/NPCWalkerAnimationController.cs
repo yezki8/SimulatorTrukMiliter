2cdd850beb31053c8f7a6630f982bb61e8803abb
yezki8 <yehezkiel.live@gmail.com>
2024-10-21 07:26:36 +0700

    Implement many NPC on Komsen city

--- Assets/Lab Metaverse/_Scripts/AI/NPCWalkerAI/NPCWalkerAnimationController.cs
+++ Assets/Lab Metaverse/_Scripts/AI/NPCWalkerAI/NPCWalkerAnimationController.cs
@@ -8,6 +8,12 @@ public class NPCWalkerAnimationController : MonoBehaviour
     [SerializeField] private float _currentSpeed;
     [SerializeField] private NavMeshAgent _nav;
     [SerializeField] private Animator _anim;
+    private Vector3 previous;
+
+    private void Start()
+    {
+        previous = transform.position;
+    }
 
     // Update is called once per frame
     void Update()
@@ -17,7 +23,9 @@ public class NPCWalkerAnimationController : MonoBehaviour
 
     void SetSpeed()
     {
-        _currentSpeed = _nav.velocity.magnitude / _nav.speed;
+
+        _currentSpeed = ((transform.position - previous).magnitude) / Time.deltaTime;
+        previous = transform.position;
         _anim.SetFloat("speed", _currentSpeed);
     }
 }
