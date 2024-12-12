287b5a44a0be5a01c9d9cd10f1e9a721b1f2a15d
yezki8 <yehezkiel.live@gmail.com>
2024-12-12 18:23:03 +0700

    Tweak convoy speed

--- Assets/Lab Metaverse/_Scripts/AI/NPCVehicle/ConvoySpeedController.cs
+++ Assets/Lab Metaverse/_Scripts/AI/NPCVehicle/ConvoySpeedController.cs
@@ -8,14 +8,18 @@ public class ConvoySpeedController : MonoBehaviour
     [SerializeField] private Transform _onFrontVehicle;
     [SerializeField] private float _distanceThreshold;
     public float CurrentOnFrontDistance;
+    private float _prevFrontDistance;
 
     [SerializeField] private ConvoySpeedController _lastCarSpeedController;
     [SerializeField] private Transform _onBehindVehicle;
     public float CurrentBehindDistance;
+    private float _prevBehindDistance;
 
     [SerializeField] [Range(1, 9)] private float _speedLimitLevel = 5;
 
     public float CurrentSpeedLimit = 0;
+
+    [SerializeField] private bool _isWaitingForVehicleBehind = true;
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +30,8 @@ public class ConvoySpeedController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        _prevFrontDistance = CurrentOnFrontDistance;
+        _prevBehindDistance = CurrentBehindDistance;
         CalculateDistance();
     }
 
@@ -50,14 +56,31 @@ public class ConvoySpeedController : MonoBehaviour
         }
         float dynamicLimit = targetLimit;
 
-        if (CurrentBehindDistance > _distanceThreshold && _onBehindVehicle !=null)
+        if (_isWaitingForVehicleBehind)
         {
-            dynamicLimit = targetLimit * (_distanceThreshold / CurrentBehindDistance);
-        }
-        else
-        {
-            dynamicLimit = targetLimit;
-        }
+            // checks: vehicle behind exist, distance threshold, currentSpeed - prevSpeed > 0f
+            if (_onBehindVehicle != null && CurrentBehindDistance > _distanceThreshold && _prevBehindDistance - CurrentBehindDistance < 1f)
+            {
+                float tempLimit = targetLimit - 
+                    ((targetLimit * ((CurrentBehindDistance - _distanceThreshold) / _distanceThreshold)) * 
+                    (_speedLimitLevel/10));
+
+
+                if (tempLimit < targetLimit / 8)
+                {
+                    dynamicLimit = targetLimit / 8;
+                }
+                else
+                {
+                    dynamicLimit = tempLimit;
+                }
+                
+            }
+            else
+            {
+                dynamicLimit = targetLimit;
+            }
+        }        
 
         CurrentSpeedLimit = dynamicLimit;
 
